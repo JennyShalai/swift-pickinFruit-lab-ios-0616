e3809b586c4e9a85a657fe5f36d768857aa6d0b6
JennyShalai <jenny.shalai@gmail.com>
2016-07-26 14:04:20 -0400

    tests passed

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
     
@@ -17,15 +18,42 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.pickerView.accessibilityLabel = Constants.FRUIT_PICKER
         // self.fruitPicker.accessibilityLabel = Constants.FRUIT_PICKER
         self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
+        self.pickerView.delegate = self
+        self.pickerView.dataSource = self
+        self.pickFruit()
+        
         
     }
     
     @IBAction func spinButtonTapped(sender: UIButton) {
-        
-        
+        self.pickFruit()
+        if (Int(self.pickerView.selectedRowInComponent(0).description)! % self.fruitsArray.count) == (Int(self.pickerView.selectedRowInComponent(1).description)! % self.fruitsArray.count) && (Int(self.pickerView.selectedRowInComponent(1).description)! % self.fruitsArray.count) == (Int(self.pickerView.selectedRowInComponent(2).description)! % self.fruitsArray.count) {
+            self.resultLabel.text = "WINNER!"
+        } else {
+            self.resultLabel.text = "TRY AGAIN"
+        }
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 100
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.fruitsArray[row % self.fruitsArray.count]
+    }
+    
+    func pickFruit() {
+        for i in 0...2 {
+            let randomInt = Int(arc4random_uniform(UInt32(100)))
+            self.pickerView.selectRow(randomInt, inComponent: i, animated: true)
+        }
     }
     
 }
